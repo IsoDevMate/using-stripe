f8f98c6bf694984359d6fdfa02c4c2f8f957eb45
IsoDevMate <oumaoduor5827@gmail.com>
2023-08-01 12:57:25 +0300

    image-not rendering corrected

--- ecom-ui/src/App.jsx
+++ ecom-ui/src/App.jsx
@@ -1,17 +1,18 @@
 import {
   BrowserRouter,
-  Link,
   Route,
   Routes,
 } from "react-router-dom";
 import { Success } from "./transc-statuspages/success";
 import { Cancel } from "./transc-statuspages/cancel";
-
+import { StripePayment } from "./components/card";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Home />} />
+        <Route path="/*" element={<StripePayment />} />
         <Route path="/success" element={<Success /> } />
         <Route path="/cancel" element={<Cancel />} />
       </Routes>
--- ecom-ui/src/components/card.jsx
+++ ecom-ui/src/components/card.jsx
@@ -1,37 +1,37 @@
 import  { useState } from 'react';
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
-import { loadStripe } from "@stripe/stripe-js";
+import axios from 'axios';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import { loadStripe } from '@stripe/stripe-js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import IMG from '../images/iphone.jpg';
 
-const StripePayment = () => {
-  const [product, setProduct] = useState({
-    name: "iphone",
+export const StripePayment = () => {
+  const [product, setProduct] = useState([{
+    name: 'iphone',
     price: 1000,
-    productOwner: "APPLE",
-    description: "THIS COMPANY OF ONE OF THE BEST DOCS",
+    productOwner: 'APPLE',
+    description: 'THIS COMPANY OF ONE OF THE BEST DOCS',
     quantity: 1,
-  });
+}]);
 
   const handlePayment = async () => {
-    const stripe = await loadStripe("YOUR_STRIPE_PUBLISHABLE_KEY");
-    const body = { product }; // Use the product state variable here
+    const stripe = await loadStripe(
+      'pk_test_51NGYDuCZYEjq9G2uxPaav5ZKYWVuVhhzrPPQDKBlUfJT8EhSfsCdq3GsVxPz012amQ904zC0lLuFrpktDrFGsJ7f006j7FHyhK'
+    );
+    const body = { product };
     const headers = {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     };
 
     try {
-      const response = await fetch(
-        "http://localhost:5005/api/checkout-session",
-        {
-          method: "POST",
-          headers: headers,
-          body: JSON.stringify(body),
-        }
-      );
+      const response = await axios.post('http://localhost:5005/api/checkout-session', body, {
+        headers: headers,
+      });
 
-      const session = await response.json();
+      const session = response.data;
 
-      const result = await stripe.redirectToCheckout({
+      const result = stripe.redirectToCheckout({
         sessionId: session.id,
       });
 
@@ -39,22 +39,37 @@ const StripePayment = () => {
         console.log(result.error);
       }
     } catch (error) {
-      console.error(error);
+      console.error('Error during payment:', error);
     }
   };
 
+  const updateProductQuantity = (newQuantity) => {
+    setProduct((prevProduct) => ({
+      ...prevProduct,
+      quantity: newQuantity,
+    }));
+  };
+
   return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Img src="https://images.pexels.com/photos/16004744/pexels-photo-16004744/free-of-apple-iphone-14-pro-max-mobile-phone.jpeg?auto=compress&cs=tinysrgb&w=600" />
+  
+    <Card style={{ width: '18rem', backgroundColor: '#f0f0f0', color: '#333', margin: '1rem' }}>
+      <Card.Img
+        variant="left"
+        src={IMG}
+      />
       <Card.Body>
         <Card.Title>{product.name}</Card.Title>
-        <Card.Text>
-          {product.description}
-        </Card.Text>
-        <Button variant="primary" onClick={handlePayment}>ADD TO CART {product.price}</Button>
+        <Card.Text>{product.description}</Card.Text>
+        <Button variant="primary" onClick={handlePayment}>
+          ADD TO CART {product.price}
+        </Button>
+        <Button variant="secondary" onClick={() => updateProductQuantity(product.quantity + 1)}>
+          Increase Quantity
+        </Button>
+        <Button variant="secondary" onClick={() => updateProductQuantity(product.quantity - 1)}>
+          Decrease Quantity
+        </Button>
       </Card.Body>
     </Card>
   );
 };
-
-export default StripePayment;
