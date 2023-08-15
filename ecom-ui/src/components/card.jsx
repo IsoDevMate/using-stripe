518e6cb04f2ac6baa9002226f49893695eb48fce
IsoDevMate <oumaoduor5827@gmail.com>
2023-08-15 13:33:17 +0300

    working on invalid product data  error

--- ecom-ui/src/components/card.jsx
+++ ecom-ui/src/components/card.jsx
@@ -8,18 +8,22 @@ import IMG from '../images/iphone.jpg';
 
 export const StripePayment = () => {
   const [product, setProduct] = useState([{
-    name: 'iphone',
-    price: 1000,
-    productOwner: 'APPLE',
-    description: 'THIS COMPANY OF ONE OF THE BEST DOCS',
+    name: 'iphone-13pro-max',
+    price: 23,
+    /*  productOwner: 'APPLE',
+    description: 'THIS COMPANY OF ONE OF THE BEST DOCS', */
     quantity: 1,
-}]);
+  }]);
+  const [isProcessing, setIsProcessing] = useState(false);
 
   const handlePayment = async () => {
+    setIsProcessing(true);
+
     const stripe = await loadStripe(
       'pk_test_51NGYDuCZYEjq9G2uxPaav5ZKYWVuVhhzrPPQDKBlUfJT8EhSfsCdq3GsVxPz012amQ904zC0lLuFrpktDrFGsJ7f006j7FHyhK'
     );
-    const body = { product };
+    //pass product details to backend
+    const body = {  product };
     const headers = {
       'Content-Type': 'application/json',
     };
@@ -37,9 +41,12 @@ export const StripePayment = () => {
 
       if (result.error) {
         console.log(result.error);
+        setIsProcessing(false);
       }
     } catch (error) {
       console.error('Error during payment:', error);
+      alert('Payment failed. Please try again later.');
+      setIsProcessing(false);
     }
   };
 
@@ -51,22 +58,22 @@ export const StripePayment = () => {
   };
 
   return (
-  
     <Card style={{ width: '18rem', backgroundColor: '#f0f0f0', color: '#333', margin: '1rem' }}>
-      <Card.Img
-        variant="left"
-        src={IMG}
-      />
+      <Card.Img variant="left" src={IMG} />
       <Card.Body>
         <Card.Title>{product.name}</Card.Title>
         <Card.Text>{product.description}</Card.Text>
-        <Button variant="primary" onClick={handlePayment}>
-          ADD TO CART {product.price}
+        <Button variant="primary" onClick={handlePayment} disabled={isProcessing}>
+          {isProcessing ? 'Processing...' : `ADD TO CART ${product.price}`}
         </Button>
         <Button variant="secondary" onClick={() => updateProductQuantity(product.quantity + 1)}>
           Increase Quantity
         </Button>
-        <Button variant="secondary" onClick={() => updateProductQuantity(product.quantity - 1)}>
+        <Button
+          variant="secondary"
+          onClick={() => updateProductQuantity(product.quantity - 1)}
+          disabled={product.quantity === 1}
+        >
           Decrease Quantity
         </Button>
       </Card.Body>
--- index.js
+++ index.js
@@ -10,43 +10,60 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
+//BODY PARSER
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-const YOUR_DOMAIN = process.env.YOUR_DOMAIN || 'https://d232-102-222-221-118.ngrok-free.app';
 
-// Routes here
-app.get('/', (req, res, next) => {
-  res.render('index page', { title: 'welcome to home Page' });
-  next();
+
+const YOUR_DOMAIN = process.env.YOUR_DOMAIN || 'https://6e1f-102-222-221-118.ngrok-free.app ';
+
+const Joi = require('joi');
+const productSchema = Joi.object({
+  id: Joi.string().required(),
+  quantity: Joi.number().integer().min(1).required(),
 });
 
+/**
+ * Callback function for the '/checkout-session' route.
+ * Creates a checkout session using the Stripe API based on the provided product data.
+ * Returns the session ID as a response.
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @returns {Object} - The response object.
+ */
 app.post('/checkout-session', async (req, res) => {
   const { product } = req.body;
 
-  if (!product || !Array.isArray(product)) {
+  // Validate product data
+  const { error } = productSchema.validate(product);
+  if (error) {
     return res.status(400).json({ message: 'Invalid product data' });
   }
+
   try {
+    // Create line items
     const lineItems = product.map((product) => ({
       price: product.id,
       quantity: product.quantity,
-    
     }));
 
+    // Create checkout session
     const session = await stripe.checkout.sessions.create({
-      
       payment_method_types: ['card'],
-      line_items: lineItems[0],
+      line_items: lineItems,
       mode: 'payment',
-     /*  payment_status: 'unpaid', */
       success_url: `${YOUR_DOMAIN}?success=true`,
       cancel_url: `${YOUR_DOMAIN}?cancel=true`,
     });
 
-    res.status(200).json({ id: session.id });
+    // Return session ID
+    return res.status(200).json({ id: session.id });
   } catch (error) {
     console.error('Error during payment:', error);
-    res.status(500).json({ message: 'Error during payment' });
+    return res.status(500).json({ message: 'Error during payment' });
   }
 });
-
+    
+// Listen
 app.listen(port, () => console.log(`server is running on port ${port}`));
